797e0beab3bbc8cd4ab13f038b32989452a0843e
vravuri13 <vamcravuri@gmail.com>
2024-04-26 21:14:34 -0500

    adding new class RewardValue

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,30 @@
+public class RewardValue {
+    private Double cash;
+    private int miles;
+
+    public RewardValue(double cash) {
+        this.cash = cash;
+    }
+
+    public RewardValue(int miles) {
+        this.miles = miles;
+    }
+
+    public Double getCashValue() {
+        try {
+            return cash + miles * 0.0035;
+        } catch (NullPointerException e) {
+            System.out.println(e);
+            return -1.00;
+        }
+    }
+
+    public int getMilesValue() {
+        try {
+            return (int) (miles + cash * (1 / 0.0035));
+        } catch (NullPointerException e) {
+            System.out.println(e);
+            return -1;
+        }
+    }
+}
